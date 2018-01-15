623957cbe057962d6b16e16486ad4296dd5895ea
Jonas Laux <jonas.laux@hotmail.com>
2018-01-15 14:16:24 +0100

    Change

--- src/de/jon4x/supportsystem/commands/SupportCommand.java
+++ src/de/jon4x/supportsystem/commands/SupportCommand.java
@@ -23,10 +23,8 @@ public class SupportCommand extends Command{
 
                 if (args.length == 0) {
                     p.sendMessage(new TextComponent(Main.getPrefix() + "§cDu §ckannst §cals §eTeammitglied §cden Support nicht verwenden!"));
-                    p.sendMessage(new TextComponent("\n§7Nutze §c/support accept <player>"));
-                    p.sendMessage(new TextComponent("§7Nutze §c/support finish"));
-                    p.sendMessage(new TextComponent("§7Nutze §c/"));
-                    return;
+                    p.sendMessage(new TextComponent("\n§7Bitte nutze §c/support accept <player>"));
+                    p.sendMessage(new TextComponent("§7Bitte nutze §c/support finish"));
                 }
 
                 if (args[0].equalsIgnoreCase("accept")) {
@@ -42,16 +40,22 @@ public class SupportCommand extends Command{
                             if (!SupportManager.getInstance().getSupportPlayer().containsValue(target)) {
                                 if (SupportManager.getInstance().getSupportQueue().contains(target)) {
                                     SupportManager.getInstance().getSupportQueue().remove(target);
-                                    target.sendMessage(new TextComponent(Main.getPrefix() + "§7Du §7wirst §7nun §7in §7einem §7privaten §7Chat §7betreut.\n§aDein §aSupporter §8\u00bb §e" + p.getName()));
-                                    p.sendMessage(new TextComponent(Main.getPrefix() + "§7Du §7betreust §7nun §7einen §7Spieler §7in §7einem §7privaten §7Chat.\n§aDein §aSpieler §8\u00bb §e" + target.getName()));
+                                    SupportManager.getInstance().getSupportPlayer().put(p, target);
+                                    target.sendMessage(new TextComponent(Main.getPrefix() + "§7Sie §7werden §7nun §7in §7einem §7privaten §7Chat §7betreut.\n§aIhr §aSupporter §8\u00bb §e" + p.getName()));
+                                    p.sendMessage(new TextComponent(Main.getPrefix() + "§7Sie §7betreuen §7nun §7einen §7Spieler §7in §7einem §7privaten §7Chat.\n§aIhr §aSpieler §8\u00bb §e" + target.getName()));
                                 }
                                 else {
                                     p.sendMessage(new TextComponent(Main.getPrefix() + "§cDieser §cSpieler §cbefindest §csich §cnicht §cin §cder §cWarteschlange!"));
                                 }
                             }
                             else {
-                                ProxiedPlayer supporter = (ProxiedPlayer) Methods.getKeyFromValue(SupportManager.getInstance().getSupportPlayer(), target);
-                                p.sendMessage(new TextComponent(Main.getPrefix() + "§cDieser §cSpieler §cwird §cbereits §cvon §e" + supporter + "§c betreut!"));
+                                if (SupportManager.getInstance().getSupportPlayer().containsValue(target)) {
+                                    ProxiedPlayer supporter = (ProxiedPlayer) Methods.getKeyFromValue(SupportManager.getInstance().getSupportPlayer(), target);
+                                    p.sendMessage(new TextComponent(Main.getPrefix() + "§cDieser §cSpieler §cwird §cbereits §cvon §e" + supporter + "§c betreut!"));
+                                }
+                                else {
+                                    p.sendMessage(new TextComponent(Main.getPrefix() + "§cDiser §cSpieler §cbefindet §csich §cnicht §cin §cder §cWarteschlange!"));
+                                }
                             }
                         }
                         else {
@@ -59,7 +63,7 @@ public class SupportCommand extends Command{
                         }
                     }
                     else {
-                        p.sendMessage(new TextComponent(Main.getPrefix() + "§cDu befindest dich bereits in einem Support-Gespräch!"));
+                        p.sendMessage(new TextComponent(Main.getPrefix() + "§cSie befinden sich bereits in einem Support-Gespräch!"));
                     }
                 }
 
@@ -77,19 +81,22 @@ public class SupportCommand extends Command{
 
             else {
                 if (args.length == 0) {
-                    SupportManager.getInstance().getSupportQueue().add(p);
-                    SupportManager.getInstance().sendToTeam(p);
-                    p.sendMessage(new TextComponent(Main.getPrefix() + "§7Du §7wurdest §7zur §aWarteschlange §7hinzugefügt.\n§7Bitte §7habe §7einen §7moment §7Geduld."));
+                    if (!SupportManager.getInstance().getSupportQueue().contains(p)) {
+                        SupportManager.getInstance().getSupportQueue().add(p);
+                        SupportManager.getInstance().sendToTeam(p);
+                        p.sendMessage(new TextComponent(Main.getPrefix() + "§aSie §awurden §azur §aWarteschlange §ahinzugefügt.\n§7Bitte §7habe §7einen §7moment §7Geduld."));
+                    }
+                    else {
+                        p.sendMessage(new TextComponent(Main.getPrefix() + "§cSie §cbefinden §csich §cbereits §cin der Warteschlange!\n§7Bitte §7habe §7einen §7moment §7Geduld."));
+                    }
                 }
                 if (args[0].equalsIgnoreCase("leave")) {
                     if (SupportManager.getInstance().getSupportQueue().contains(p)) {
                         p.sendMessage(new TextComponent(Main.getPrefix() + "§cSie haben die Warteschlange verlassen!"));
                         SupportManager.getInstance().getSupportQueue().remove(p);
                     }
-                    else if (SupportManager.getInstance().getSupportPlayer().containsValue(p)) {
-                        ProxiedPlayer target = (ProxiedPlayer) Methods.getKeyFromValue(SupportManager.getInstance().getSupportPlayer(), p);
-                        SupportManager.getInstance().getSupportPlayer().remove(p);
-                        SupportManager.getInstance().getSupportPlayer().remove(target);
+                    else {
+                        p.sendMessage(new TextComponent(Main.getPrefix() + "§cSie befinden sich nicht in der Warteschlange!"));
                     }
                 }
             }
